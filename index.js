fbd415cba7013a25a8267d2af0ab3a354aebc92e
Movin Vinusandha <movinvinusandha@gmail.com>
2025-06-04 14:07:02 +0530

    Add feature
    Setting the website default location to the user's current location

--- index.js
+++ index.js
@@ -37,11 +37,32 @@ function unixTimeConvert(timestamp){
     return timeFormat;
 }
 
+// geting current location using user public ip address
+async function currentLocation(getIp){
+    let ip = getIp;
+    
+    // If you are using localhost, change its IP address to a real IP address
+    if (ip == "::1"){
+        ip = "157.245.55.252"   // put your actual ip address
+    }
+    
+    const ipLocationURL = `https://get.geojs.io/v1/ip/geo/${ip}.json`;
 
-// current weather forcast defalt location
+    response3 = await axios.post(ipLocationURL);
+    lat = response3.data.latitude
+    lon = response3.data.longitude
+}
+
+
+// current weather forcast
 app.get("/", async (req, res) => {
+
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    currentLocation(ip);
+
     const URL1 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`;
     const URL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}&units=metric&cnt=6`;
+    
     try{
         response1 = await axios.post(URL1);
         response2 = await axios.post(URL2);
@@ -52,7 +73,7 @@ app.get("/", async (req, res) => {
             sunset: unixTimeConvert(response1.data.sys.sunset)
         }
         
-        
+
 
         res.render("index.ejs", {
             content: response1.data,
